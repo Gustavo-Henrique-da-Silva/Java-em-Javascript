672b10f29088f9f45717763e268949f622331ce9
Gustavo-Henrique-da-Silva <gustavohenrique10002@gmail.com>
2022-09-19 14:46:15 -0300

    Concluido a atividade de desenvolver carros no JS

---
+++ main.js
@@ -0,0 +1,42 @@
+
+
+
+let c1 = new Carro(new MotorFusca(), "Fusca", "Volkswagen", "1956","Preto");
+let c2 = new Carro(new MotorCelta(), "Celta", "Chevrolet", "1995","Azul");
+let c3 = new Carro(new MotorCamaro(), "Camaro", "Chevrolet", "2006","Amarelo");
+let c4 = new Carro(new MotorFordKa(), "Ka", "Ford", "2001","Verde");
+let c5 = new Carro(new MotorFerrari(), "Berlinetta", "Ferrari", "2008","Vermelho");
+let c6 = new Carro(new MotorLamborghini(), "Huracan", "Lamborghini", "2017","Vermelho");
+console.log(c1.marca + " " + c1.nome + " " + c1.cor + " " +  c1.ano + ": " + c1.acelerar());
+console.log(c2.marca + " " + c2.nome + " " + c2.cor + " " +  c2.ano + ": " + c2.acelerar());
+console.log(c3.marca + " " + c3.nome + " " + c3.cor + " " +  c3.ano + ": " + c3.acelerar());
+console.log(c4.marca + " " + c4.nome + " " + c4.cor + " " +  c4.ano + ": " + c4.acelerar());
+console.log(c5.marca + " " + c5.nome + " " + c5.cor + " " +  c5.ano + ": " + c5.acelerar());
+console.log(c6.marca + " " + c6.nome + " " + c6.cor + " " +  c6.ano + ": " + c6.acelerar());
+
+     
+
+
+function rodadepoisdecarregarobody(){
+   let minhatagdiv = document.getElementById('tagdiv');
+	console.log("rodadepoisdecarregarobody");
+	console.log(minhatagdiv);
+	console.log("rodadepoisdecarregarobody");
+}
+console.log(minhafuncao1);
+let x = 5;
+let y = "Sgueide";
+
+function minhafuncao1(){
+
+
+	console.log("rodou minhafuncao1" );
+}
+
+console.log(typeof(minhafuncao1));
+console.log(typeof(x))
+console.log(typeof(y))
+console.log(minhafuncao1);
+
+
+
